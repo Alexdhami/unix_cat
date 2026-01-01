19e2a73732783b6d0d9e59f03d8bce6fa8f21616
alexdhami <b2ok369@proton.me>
2026-01-01 12:07:54 +0545

    Initial commit

---
+++ open_file.asm
@@ -0,0 +1,97 @@
+; CONSTANTS
+
+ENOENT equ -2 ; file did not find error
+; syscalls
+
+SYS_WRITE  equ 1
+SYS_READ   equ 0   
+SYS_EXIT   equ 60 
+SYS_OPENAT equ 257 
+
+; fd (file descripters)
+STDOUT     equ 1
+AT_FDCWD   equ -100 
+
+
+section .data
+    file_not_found db "File not found, good luck bro", 10
+    file_not_found_len equ $-file_not_found
+
+    specify_filename db "Please for the sake of the God specify the file name to read", 10
+    specify_filename_len equ $-specify_filename
+
+section .text
+    global _start
+
+_start:
+    ; [rsp] = agrc, [rsp+8] = argv[0](filename), [rsp+16] = argv[1](filename to open)-> second arg
+    mov rcx, [rsp] ; now rcx = argc
+    cmp rcx, 2 ; compare rcx with 2
+    ; if rcx < 2
+    jb no_arg ; (jump if below) to no_arg
+    ; else
+    mov rsi, [rsp+16]   ; pathname | point rsi to first character of the filename(argv[1])
+
+    mov rax, SYS_OPENAT ; int openat(int dirfd, const char *pathname, int flags, mode_t mode);
+    mov rdi, AT_FDCWD   ; dirfd    | AT_FDCWD (relativce to curent working dir)
+    mov rdx, 0          ; flags    | R_DONLY
+    mov r9,  0          ; mode
+    syscall
+
+    mov r9, rax ; syscall returns fd in rax so save it in r9
+    cmp r9, ENOENT  ; -2 = ENOENT (file not found) error
+    ; if r9 == ENOENT(-2) 
+    je FNF ; file not found 
+    jmp read_loop
+
+read_loop:
+    ; Read and save to the buffer
+    mov rax, SYS_READ  ; sys_read
+    mov rdi, r9        ; fd
+    mov rsi, file_info ; save the 1024(specified in rdx reg) bytes to the buffer(file_info)
+    mov rdx, 1024      ; upto 1024 bytes to read
+    syscall
+
+    ; if rax == 0:
+    cmp rax, 0 ; check if read returned 0 bytes (EOF) 
+    jle pass_exit
+
+    ; else:
+    mov rdx, rax       ; mov the total bytes read in rdx 
+
+    ; Write
+    mov rax, SYS_WRITE  ; sys_read
+    mov rdi, STDOUT    ; standard_out
+    mov rsi, file_info
+    syscall
+    jmp read_loop
+    
+no_arg:
+    mov rax, SYS_WRITE ; sys_write
+    mov rdi, STDOUT    ; stdout
+    mov rsi, specify_filename
+    mov rdx, specify_filename_len
+    syscall
+    jmp failure_exit
+
+FNF:
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT 
+    mov rsi, file_not_found
+    mov rdx, file_not_found_len
+    syscall
+    jmp failure_exit
+
+pass_exit:
+    ; exit
+    mov rax, SYS_EXIT ; sys_exit
+    xor rdi, rdi ; fd = 0, success
+    syscall
+
+failure_exit:
+    mov rax, SYS_EXIT
+    mov rdi, 1 ; fd = 1, failure
+    syscall
+
+section .bss
+    file_info resb 1024
