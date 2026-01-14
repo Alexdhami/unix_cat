785abb080cf864815536cca040ce22f37a39a46d
alexdhami <b2ok369@proton.me>
2026-01-14 10:56:46 +0545

    Refactor cat-like syscall assembly: - Cleaned up read/write loop - Improved error handling with separate labels - Added printf macro for clarity - Made messages more user-friendly - Organized registers and comments

--- cat.asm
+++ cat.asm
@@ -1,11 +1,13 @@
 ; CONSTANTS
 
-ENOENT equ -2  ; file did not find error
-EACCES equ -13 ; could not access file
+; errors
+ENOENT     equ -2  ; file did not find error
+EACCES     equ -13 ; could not access file
 
 ; syscalls
 SYS_WRITE  equ 1
 SYS_READ   equ 0   
+SYS_CLOSE  equ 3
 SYS_EXIT   equ 60 
 SYS_OPENAT equ 257 
 
@@ -13,114 +15,109 @@ SYS_OPENAT equ 257
 STDOUT     equ 1
 AT_FDCWD   equ -100 
 
+%macro printf 2 ; 1 = pointer to str, 2 = len(str)
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, %1
+    mov rdx, %2
+    syscall
+%endmacro
+
 section .data
+    msg_not_found db "File not found :(", 10
+    len_not_found equ $-msg_not_found
 
-    file_not_found db "File not found, good luck bro", 10
-    file_not_found_len equ $-file_not_found ; Length of the msg
+    msg_no_filename db "No file to open. Usage: cat <filename>", 10
+    len_no_filename equ $-msg_no_filename 
 
-    specify_filename db "Please for the sake of the God specify the file name to read", 10
-    specify_filename_len equ $-specify_filename 
+    msg_no_access db "Please check your file permissions! ", 10
+    len_no_access equ $-msg_no_access
 
-    no_file_access db "Please check your file permissions! ", 10
-    no_file_access_len equ $-no_file_access
+    msg_hint db "Hint: does your file contains read permissions?", 10
+    len_hint equ $-msg_hint
 
-    hint db "hint: does your file contains read permissions?", 10
-    hint_len equ $-hint
+    msg_generic_error db "Could not open the file :(", 10
+    len_generic_error equ $-msg_generic_error
 
 section .text
     global _start
 
 _start:
-    ; [rsp] = agrc, [rsp+8] = argv[0](filename), [rsp+16] = argv[1](filename to open)-> second arg
-    mov rcx, [rsp] ; now rcx = argc
-    cmp rcx, 2 ; compare rcx with 2
-    ; if rcx < 2
-    jb no_arg ; (jump if below) to no_arg
-    ; else
-    mov rsi, [rsp+16]   ; pathname | point rsi to first character of the filename(argv[1])
+    check_argc:
+        ; [rsp] = agrc, [rsp + 8] = argv[0], [rsp + 16] = argv[1]
+        mov rcx, [rsp]
 
-    mov rax, SYS_OPENAT ; int openat(int dirfd, const char *pathname, int flags, mode_t mode);
-    mov rdi, AT_FDCWD   ; dirfd    | AT_FDCWD (relativce to curent working dir)
-    mov rdx, 0          ; flags    | R_DONLY
-    mov r9,  0          ; mode
-    syscall
+        cmp rcx, 2    ; compare rcx with 2
+        jb no_arg     ; jump if below
+        jmp open_file
 
-    mov r9, rax ; syscall returns fd in rax so save it in r9
-    cmp rax, ENOENT  ; -2 = ENOENT (file not found) error
-    ; if r9 == ENOENT(-2) 
-    je FNF ; file not found 
+    open_file:
+        mov rax, SYS_OPENAT   ; size_t openat(dir_fd, pathname, flag(O_RDONLY, O_RDWR), mode )
+        mov rdi, AT_FDCWD     ; first arguement
+        mov rsi, [rsp+16]     ; second arguement  -> rsi points to argv[1]
+        mov rdx, 0            ; third arguement   ->  R_DONLY
+        mov r10, 0            ; used only when creating a new file (e.g., 0666 for read/write permissions). Not used here
+        syscall
+
+        cmp rax, ENOENT
+        je file_not_found     ; jmp if equal
+        
+        cmp rax, EACCES 
+        je permissions_denied ; jmp if equal
+
+        test rax, rax
+        js generic_error     ; jump if signed bit
+
+        mov r12, rax          ; syscall returns fd in rax. So, save it in r12
+        jmp read_loop
+
+    read_loop:
+        ; Read and save to the buffer
+        mov rax, SYS_READ      
+        mov rdi, r12          ; fd
+        mov rsi, file_content 
+        mov rdx, 1024 
+        syscall
+
+        ; rax returns how many bytes it read from the file
+        cmp rax, 0 
+        jle successful_exit   ; <= 0 bytes read then completed reading
+
+        mov r13, rax          ; mov the total bytes read in r13 
+        printf file_content, r13 
+        jmp read_loop
+        
+    no_arg:
+        printf msg_no_filename, len_no_filename
+        jmp failure_exit
+
+    file_not_found:
+        printf msg_not_found, len_not_found
+        jmp failure_exit
+
+    permissions_denied:
+        printf msg_no_access, len_no_access
+        printf msg_hint, len_hint
+        jmp failure_exit
     
-    ; elif r9 = EACCES(-13)
-    cmp rax, EACCES 
-    je permissions_denied
+    generic_error:
+        printf msg_generic_error, len_generic_error
+        jmp failure_exit
 
-    ; else:
-    jmp read_loop
+    failure_exit:
+        mov rax, SYS_EXIT
+        mov rdi, 1       ; fd = non-zero -> failure     
+        syscall
 
-read_loop:
-    ; Read and save to the buffer
-    mov rax, SYS_READ  ; sys_read
-    mov rdi, r9        ; fd
-    mov rsi, file_info ; save the 1024(specified in rdx reg) bytes to the buffer(file_info)
-    mov rdx, 1024      ; upto 1024 bytes to read
-    syscall
+    successful_exit:
+        .close_file:
+            mov rax, SYS_CLOSE 
+            mov rdi, r12 ; rdi needs fd
+            syscall
 
-    ; if rax == 0:
-    cmp rax, 0 ; check if read returned 0 bytes (EOF) 
-    jle pass_exit
-
-    ; else:
-    mov rdx, rax       ; mov the total bytes read in rdx 
-
-    ; Write
-    mov rax, SYS_WRITE  ; sys_read
-    mov rdi, STDOUT    ; standard_out
-    mov rsi, file_info
-    syscall
-    jmp read_loop
-    
-no_arg:
-    mov rax, SYS_WRITE ; sys_write
-    mov rdi, STDOUT    ; stdout
-    mov rsi, specify_filename
-    mov rdx, specify_filename_len
-    syscall
-    jmp failure_exit
-
-FNF:
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT 
-    mov rsi, file_not_found
-    mov rdx, file_not_found_len
-    syscall
-    jmp failure_exit
-
-permissions_denied:
-    ; write no file access msg
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, no_file_access
-    mov rdx, no_file_access_len
-    syscall
-
-    ; write hint msg
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, hint
-    mov rdx, hint_len
-    syscall
-    jmp failure_exit
-
-pass_exit:
-    ; exit
-    mov rax, SYS_EXIT ; sys_exit
-    xor rdi, rdi ; fd = 0, success
-    syscall
-
-failure_exit:
-    mov rax, SYS_EXIT
-    mov rdi, 1 ; fd = 1, failure
-    syscall
-
+        .exit:
+            mov rax, SYS_EXIT 
+            xor rdi, rdi ; fd =   zero   -> success
+            syscall
 section .bss
-    file_info resb 1024
+    file_content resb 1024
